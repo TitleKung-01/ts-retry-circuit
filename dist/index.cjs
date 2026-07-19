ce8dbde51236991afbfcbc0d72f68e4fd8f4f6cf
TitleKung-01 <dev.meoww@gmail.com>
2026-07-19 21:38:57 +0700

    feat: add React useCircuitBreaker hook with shared state support and testing

--- dist/index.cjs
+++
@@ -1,1 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});var _chunk2DEGBCMEcjs = require('./chunk-2DEGBCME.cjs');exports.CircuitBreaker = _chunk2DEGBCMEcjs.a;
--- dist/index.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-JTVZSNSI.js";export{a as CircuitBreaker};
